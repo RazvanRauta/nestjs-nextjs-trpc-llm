ce7bf0540cc51de001109ec76fa970b30895e339
Razvan <40596596+RazvanRauta@users.noreply.github.com>
2025-02-06 13:09:04 +0100

    add Prettier and ESLint configuration for code formatting

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,38 +1,38 @@
-'use client'
-import { trpc } from '@web/utils/trpc'
+'use client';
+import { trpc } from '@web/utils/trpc';
 
 export default function Home() {
-	const name = `Răzvan Răuță`
-	const { isLoading, error, data } = trpc.iceBreaker.getIceBreaker.useQuery({
-		name,
-	})
+  const name = `Răzvan Răuță`;
+  const { isLoading, error, data } = trpc.iceBreaker.getIceBreaker.useQuery({
+    name,
+  });
 
-	if (isLoading) {
-		return <div>Loading...</div>
-	}
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-	if (error) {
-		return <div>Error: {error.message}</div>
-	}
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
-	if (!data) {
-		return <div>No data</div>
-	}
+  if (!data) {
+    return <div>No data</div>;
+  }
 
-	return (
-		<div>
-			<h3>{name}</h3>
+  return (
+    <div>
+      <h3>{name}</h3>
 
-			<h3>Ice Breaker</h3>
+      <h3>Ice Breaker</h3>
 
-			<p> Summary</p>
-			<p>{data.result.summary}</p>
+      <p> Summary</p>
+      <p>{data.result.summary}</p>
 
-			<ul>
-				{data.result.facts.map((fact, index) => (
-					<li key={index}>{fact}</li>
-				))}
-			</ul>
-		</div>
-	)
+      <ul>
+        {data.result.facts.map((fact, index) => (
+          <li key={index}>{fact}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- apps/web/src/providers/trpc-provider.tsx
+++ apps/web/src/providers/trpc-provider.tsx
@@ -1,50 +1,50 @@
-'use client'
+'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { httpBatchLink, loggerLink, getFetch } from '@trpc/client'
-import { trpc } from '@web/utils/trpc'
-import { useState } from 'react'
-import superjson from 'superjson'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { httpBatchLink, loggerLink, getFetch } from '@trpc/client';
+import { trpc } from '@web/utils/trpc';
+import { useState } from 'react';
+import superjson from 'superjson';
 
 const queryClient = new QueryClient({
-	defaultOptions: { queries: { staleTime: 60 * 1000 } },
-})
+  defaultOptions: { queries: { staleTime: 60 * 1000 } },
+});
 
 export default function TrpcProvider({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode;
 }) {
-	const url = `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`
+  const url = `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`;
 
-	const [trpcClient] = useState(() =>
-		trpc.createClient({
-			links: [
-				loggerLink({
-					enabled: () => true,
-				}),
-				httpBatchLink({
-					url,
-					fetch: async (input, init?) => {
-						const fetch = getFetch()
-						return fetch(input, {
-							...init,
-							// credentials: 'include',
-						})
-					},
-					transformer: superjson,
-				}),
-			],
-		})
-	)
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        loggerLink({
+          enabled: () => true,
+        }),
+        httpBatchLink({
+          url,
+          fetch: async (input, init?) => {
+            const fetch = getFetch();
+            return fetch(input, {
+              ...init,
+              // credentials: 'include',
+            });
+          },
+          transformer: superjson,
+        }),
+      ],
+    }),
+  );
 
-	return (
-		<trpc.Provider client={trpcClient} queryClient={queryClient}>
-			<QueryClientProvider client={queryClient}>
-				<ReactQueryDevtools initialIsOpen={false} client={queryClient} />
-				{children}
-			</QueryClientProvider>
-		</trpc.Provider>
-	)
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} client={queryClient} />
+        {children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 }
